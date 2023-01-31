7292f1ec22c9816f0f926f9c1ca1a305a08de119
guillepaivag <guillepaivag@gmail.com>
2023-01-31 18:56:48 -0300

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,38 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    app: {
+        head: {
+            charset: 'utf-16',
+            title: 'Consultoría Legal',
+            link: [
+                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' },
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            ],
+            meta: [
+                { name: 'description', content: 'My amazing site.' },
+            ],
+        }
+    },
+    css: [
+        'vuetify/lib/styles/main.sass', 
+        // '@mdi/font/css/materialdesignicons.min.css'
+        '@/assets/css/style.css',
+        '@/assets/css/proseMirror.less'
+    ],
+    build: {
+        transpile: ['vuetify']
+    },
+    routeRules: {
+        '/': { ssr: true },
+        '/servicio/**': { ssr: true },
+        
+    },
+    nitro: {
+        preset: 'firebase'
+    },
+    runtimeConfig: {
+        public: {
+            MODO_PROYECTO: 'dev'
+        }
+    }
+})
